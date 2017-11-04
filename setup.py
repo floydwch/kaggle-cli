2cb9659320e5c68e82d966839d588bd10c9e05f7
Alexander Fridman <AlexFridman@outlook.com>
2017-11-04 20:46:49 +0300

    Up bs4 required version
    Not worked for me before update bs4 to the latest version.

--- setup.py
+++ setup.py
@@ -45,7 +45,8 @@ setup(
         'lxml>=4.0.0,<4.1',
         'cssselect>=1.0.1,<1.1',
         'configparser',
-        'progressbar2>=3.34.3,<3.35'
+        'progressbar2>=3.34.3,<3.35',
+        'beautifulsoup4>=4.6.0'
     ],
 
     namespace_packages=[],
